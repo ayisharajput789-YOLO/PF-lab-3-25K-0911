e8be29e27c59dc99a4a9c9a61ba1dec5c576f7be
ayisharajput789-YOLO <ayisharajput789@gmail.com>
2025-09-10 18:44:50 +0500

    ayesha

--- introo.c
+++ introo.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
-int main(){
+int main() {
     int age;
-    char name=[100];
-    printf("Enter your name");
-    printf("Enter your age");
-    scanf(" Hello %s your are %d years old",name,age);
+    char name[100];
+
+    printf("Enter your name: ");
+    scanf("%s", name);   // <-- added semicolon
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    printf("Hello %s, you are %d years old\n", name, age);
+
     return 0;
 }
